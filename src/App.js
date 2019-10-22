caa6e6418770e57c3bf98ac006e5d6de22dba4c3
olugashi <orenlugashi@gmail.com>
2019-10-22 15:59:39 +0300

    Add WebMapServiceImageryProvider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
-import { Viewer } from 'resium';
+import { Viewer , ImageryLayer } from 'resium';
+import { WebMapServiceImageryProvider } from "cesium";
+
+
 
 function App() {
   return (
-    <Viewer full />
+    <Viewer full>
+    <ImageryLayer
+      imageryProvider={
+        new WebMapServiceImageryProvider({
+          url: "//services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer",
+          layers: '1',
+         
+        })
+      }
+    />
+    
+  </Viewer>
   );
 }
 
